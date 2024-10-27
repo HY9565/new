dc069093fa2d4d1310bf6f8a9488ac8df301d2bb
DESKTOP-I4Q2UMB\user <hahaha2227@naver.com>
2024-10-27 14:33:21 +0900

    edit 2

---
+++ new111/src/main/java/com/example/demo/New111Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class New111Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(New111Application.class, args);
+	}
+
+}
